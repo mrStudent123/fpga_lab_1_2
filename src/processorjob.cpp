c2d89330e121a4d20367f4c50d992ac674da92d0
Joe <sclab64@ubuntu.(none)>
2016-05-11 20:54:36 +0200

    continue work

--- src/processorjob.cpp
+++ src/processorjob.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "processorjob.h"
+#include "processor.h"
 
 processor_job::processor_job() {
    type = JOB_TYPE_NONE;
@@ -17,5 +18,26 @@ processor_job::processor_job() {
 
 processor_job::~processor_job() {
    // TODO Auto-generated destructor stub
+}
+
+processor_instruction *processor_job::getInstructions(){
+
+   processor_instruction instructions[2];
+   if(type == JOB_TYPE_ADD){
+      instructions[1].instruction = INSTR_SETA;
+      instructions[1].data = data1;
+
+      instructions[2].instruction = INSTR_ADD;
+      instructions[2].data = data2;
+   }
+   else if(type == JOB_TYPE_MUL){
+      instructions[1].instruction = INSTR_SETA;
+      instructions[1].data = data1;
+
+      instructions[2].instruction = INSTR_MUL;
+      instructions[2].data = data2;
+   }
+
+   return instructions;
 }
 
