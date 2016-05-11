771920ba4ad70eae323ae38307fdb88d47cb2168
berni <sclab64@ubuntu.(none)>
2016-05-11 15:58:04 +0200

    stim.h + main.h

--- module/stim.h
+++ module/stim.h
@@ -4,22 +4,33 @@
 
 #include "systemc.h"
 #include <math.h>
-#include "matrix.h"
+#include "../src/matrix.h"
 
+//#define number_testmatrix 100
+//#define number_outputmatrix number_testmatrix/2
+
+matrix input_array(2,2),[4];
+matrix output_array[50](2,2);
+int a[5];
 
 SC_MODULE(stim) {
-   matrix mtrix;
 
    SC_CTOR(stim) {
+      //for(int i = 0; i < 100; i++){
+         //input_array[i]
+      //}
 
       SC_THREAD(go);
 
    }
 
    void go(){
-      wait(2,SC_US);
+      matrix* mtrix = new matrix(2,2);
+      sc_fifo< processor_instruction > channel(16);
 
+      wait(2,SC_NS);
 
+      //mtrix->fillRandom(10);
 
    }
 
