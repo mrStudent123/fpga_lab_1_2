bbe5eade3ae3f87afd789ab0bbb8f489a561faad
Joe <sclab64@ubuntu.(none)>
2016-05-11 21:11:08 +0200

    fix errors

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,13 @@
 int sc_main(int argc, char* argv[]){
 
    channel_fifo_matrix inputFifo;
+   channel_fifo_matrix outputFifo;
 
    sc_clock clk ("clk", 2, SC_US);   // a clock with a period of 2 µ-sec
+
+   stim stim_inst("stim");
+   stim_inst.data_in(outputFifo);
+   stim_inst.data_out(inputFifo);
 
    matrix_multiplicator multiplicator ("multipl");
    multiplicator.clk(clk);
@@ -20,9 +25,9 @@ int sc_main(int argc, char* argv[]){
    printf("start program");
 
    sc_trace_file *tf;          // Signal tracing
-      tf=sc_create_vcd_trace_file("multicore");  // create new trace file
-      tf->set_time_unit(1,SC_US);        // set time resolution to 0.5 �-sec
-      //sc_trace(tf, amplitude_c, "Amplitude_c");
+   tf=sc_create_vcd_trace_file("multicore");  // create new trace file
+   tf->set_time_unit(1,SC_US);        // set time resolution to 0.5 �-sec
+   //sc_trace(tf, amplitude_c, "Amplitude_c");
 
 
    sc_start(2000, SC_MS);
