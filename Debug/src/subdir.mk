1ce541d75bb70adecd18e558f06d036a58590725
Joe <sclab64@ubuntu.(none)>
2016-05-11 13:48:12 +0200

    add matrix class

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,20 +4,23 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/main.cpp 
+../src/main.cpp \
+../src/matrix.cpp 
 
 OBJS += \
-./src/main.o 
+./src/main.o \
+./src/matrix.o 
 
 CPP_DEPS += \
-./src/main.d 
+./src/main.d \
+./src/matrix.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/sclab64/workspace/l1_2_multicore/module" -I/home/sclab64/Applications/systemc-2.3.1/include -O0 -g3 -Wall -c -fmessage-length=0 -fpermissive -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/sclab64/workspace/lab1_2/module" -I/home/sclab64/Applications/systemc-2.3.1/include -O0 -g3 -Wall -c -fmessage-length=0 -fpermissive -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
---
+++ src/matrix.cpp
@@ -0,0 +1,68 @@
+/*
+ * matrix.cpp
+ *
+ *  Created on: May 11, 2016
+ *      Author: sclab64
+ */
+
+#include "matrix.h"
+#include <stdlib.h> /* fuer zufallszahlen */
+
+matrix::matrix(int width, int height) {
+   w = width;
+   h = height;
+
+   short matrix_data[width*height];
+   data = matrix_data;
+
+   for(int i=0; i<w*h; i++){
+      data[i] = 0;
+   }
+}
+
+matrix::~matrix() {
+   // TODO Auto-generated destructor stub
+}
+
+short matrix::get(int x, int y){
+   if(x > w || y > h){
+      return 0;
+   }
+
+   return data[y*w + x];
+}
+
+bool matrix::put(int x, int y, short value){
+   if(x>w || y > h){
+      return false;
+   }
+
+   data[y*w + x] = value;
+   return true;
+}
+
+void matrix::add(matrix m){
+   for(int i=0; i<m*w; i++){
+      data[i] += m.data[i];
+   }
+}
+
+void matrix::multiply(matrix m){
+   matrix result(h,m.w);
+
+   for(int result_x=0; result_x < result.w; result_x++)
+   {
+      for(int result_y=0; result_y < result.h; result_y++)
+      {
+         for(int i=0; i<w && i < m.h; i++){
+            result.data[result_y*result.w + result_x] += get(result_y,i) * m.get(i,result_x);
+         }
+      }
+   }
+}
+
+void matrix::fillRandom(short max){
+   for(int i=0; i<w*h; i++){
+      data[i] = rand() % max;
+   }
+}
