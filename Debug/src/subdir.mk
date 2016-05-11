d84952360b80f3de961d124c02a61288ac839172
berni <sclab64@ubuntu.(none)>
2016-05-11 13:50:42 +0200

    asdf

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/sclab64/workspace/l1_2_multicore/module" -I/home/sclab64/Applications/systemc-2.3.1/include -O0 -g3 -Wall -c -fmessage-length=0 -fpermissive -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/sclab64/workspace/git_clone/module" -I/home/sclab64/Applications/systemc-2.3.1/include -O0 -g3 -Wall -c -fmessage-length=0 -fpermissive -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
